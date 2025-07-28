791adb4b9522ed2ed6af73bb8ffd643a9c75337b
harshal2223 <harshalsharma123456@gmail.com>
2025-07-28 23:09:13 +0530

    Set up basic App component to render HomePage
    - Simplified App to directly render HomePage component
    - Prepared main entry point for the application

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,28 +1,8 @@
 import React from 'react';
-import { ThemeProvider } from './contexts/ThemeContext';
-import { NewsletterProvider } from './contexts/NewsletterContext';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import FeaturedArticles from './components/FeaturedArticles';
-import NewsletterSignup from './components/NewsletterSignup';
-import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return (
-    <ThemeProvider>
-      <NewsletterProvider>
-        <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
-          <Header />
-          <main>
-            <Hero />
-            <FeaturedArticles />
-            <NewsletterSignup />
-          </main>
-          <Footer />
-        </div>
-      </NewsletterProvider>
-    </ThemeProvider>
-  );
+  return <HomePage />;
 }
 
 export default App;
